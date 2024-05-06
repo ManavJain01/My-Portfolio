e323c213e8460b62baca17dbd2264b75cedac551
Manav Jain <118716368+ManavJain01@users.noreply.github.com>
2024-05-07 01:57:56 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import { useState } from 'react'
+import {Route, Routes} from 'react-router-dom'
+
+//      * Portfolio *
+import Portfolio from './Portfolio/components/Main'
+
+//      * More Projects Page *
+import More_Projects from './More Projects Page/components/Index'
+
+//      * Page Not Found *
+import Page404 from './Page 404/components/Page404'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div>
-hi
+      <Routes>
+          {/* Main Page */}
+          <Route path="/" element={<Portfolio />}></Route>
+
+          {/* Portfolio */}
+          <Route path="/More-Projects" element={<More_Projects />}></Route>
+ 
+          {/* Page Not Found */}
+          <Route path="/*" element={<Page404 />}></Route>
+        </Routes>
     </div>
   )
 }
