8fa6eba02123c87ede2f7e9ba9a3478b443c0ad7
Manav Jain <118716368+ManavJain01@users.noreply.github.com>
2024-05-07 02:20:32 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,6 @@ import {Route, Routes} from 'react-router-dom'
 //      * Portfolio *
 import Portfolio from './Portfolio/components/Main'
 
-//      * More Projects Page *
-import More_Projects from './More Projects Page/components/Index'
-
 //      * Page Not Found *
 import Page404 from './Page 404/components/Page404'
 
@@ -15,9 +12,6 @@ function App() {
       <Routes>
           {/* Main Page */}
           <Route path="/" element={<Portfolio />}></Route>
-
-          {/* Portfolio */}
-          <Route path="/More-Projects" element={<More_Projects />}></Route>
  
           {/* Page Not Found */}
           <Route path="/*" element={<Page404 />}></Route>
