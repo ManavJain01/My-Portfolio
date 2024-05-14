99eb943d676052b0fbc75274aa92763f97d8ce00
Manav Jain <118716368+ManavJain01@users.noreply.github.com>
2024-05-14 20:55:07 +0530

    commit

--- src/Portfolio/components/Footer.jsx
+++ src/Portfolio/components/Footer.jsx
@@ -1,6 +1,32 @@
+// Importing React Icons
+import { FaLinkedin } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa";
+
+//Importing React Packages
+import { Link } from "react-router-dom";
+
 function Footer(){
   return (
-    <div></div>
+    <div className="bg-black h-[30vh] w-[100vw] text-white flex justify-around px-10 py-5">
+      <div className="flex flex-col justify-center gap-3">
+        <p className="font-semibold text-2xl">Projects</p>
+        <ul className="text-gray-400 flex flex-col gap-3">
+          <li><Link to="https://mern-projects-amber.vercel.app/">MERN Projects</Link></li>
+          <li><Link to="https://manavjain01.github.io/BlockChain-Projects/MainFile/">BlockChain Projects</Link></li>
+          <li><Link to="https://more-projects-page.vercel.app/">More Projects</Link></li>
+        </ul>
+      </div>
+      
+      <div className="flex flex-col gap-3 items-center justify-center">
+        <p className="font-semibold text-2xl">CONNECT</p>
+        <section className="flex gap-5">
+          <FaInstagram className="w-8 h-8 text-gray-400" />
+          <FaLinkedin className="w-8 h-8 text-gray-400" />
+          <FaGithub className="w-8 h-8 text-gray-400" />
+        </section>
+      </div>
+    </div>
   )
 }
 
--- src/Portfolio/components/Main.jsx
+++ src/Portfolio/components/Main.jsx
@@ -1,15 +1,18 @@
 import Navbar from './Navbar';
+import Footer from './Footer'
 
 function Main(){
   return(
     <>
       <Navbar />
-      <header className="bg-black md:bg-gradient-to-r md:from-indigo-500 md:from-10% md:via-sky-500 md:via-30% md:to-emerald-500 md:to-90% h-20 md:h-36">
-        <div className="/*bg-blue-900 bg-gradient-to-r from-[#09203f] to-[#537895] h-[200vh] /h-[82vh] w-[100vw] flex flex-col items-center justify-center text-white">
+      <main className="bg-black md:bg-gradient-to-r md:from-indigo-500 md:from-10% md:via-sky-500 md:via-30% md:to-emerald-500 md:to-90% min-h-[100vh]">
+        <div className="/*bg-blue-900 bg-gradient-to-r from-[#09203f] to-[#537895] h-[82vh] w-[100vw] flex flex-col items-center justify-center text-white">
           <h1 className="text-6xl font-bold">Building...</h1>
           <h1 className="text-6xl font-bold">Come Back Later To Find out</h1>
         </div>
-      </header>
+      </main>
+
+      <Footer />
     </>
   )
 }
